0cc18a3c1ddd5b7b450d2d5429d382d36e099404
DESKTOP-JQ3H9OS\User <mateoacj@gmail.com>
2024-08-19 22:57:18 +0100

    forth commit,options 3-5  working, Exepsions missing. have to format date and numbers correctly, maybe cleaning cmd after action.

--- Expense_Tracker.py
+++ Expense_Tracker.py
@@ -2,7 +2,7 @@ class Expense:
     def __init__(self, date, description, amount):
         self.date = date
         self.description = description
-        self.amoun = amount
+        self.amount = amount
         
 class ExpenseTracker:
     def __init__(self):
@@ -13,6 +13,7 @@ class ExpenseTracker:
         self.expenses.append(expense)
     
     def removeExpense(self, index):
+        index-=1 #Lowers one from the item, so it avoids typing 0 for item 1 and so on.
         if 0 <= index < len(self.expenses):
             del self.expenses[index]
             print("Expense removed successfully")
@@ -27,11 +28,11 @@ class ExpenseTracker:
         else:
             print("Expenses list:")
             for i, expense in enumerate(self.expenses, start=1):
-                print(f"{i}, Date: {expense.date}, Description:  {expense.description}, Amount: {expense.amount:.2f}")   
+                print(f"{i} --> Date: {expense.date}, Expense Description:  {expense.description}, Amount: {expense.amount}")   
                 
     def totalExpenses(self):
-        total = sum(expense.amount for expense in self.expenses)
-        print(f"Total expenses: ${total:.2f}")
+        total = sum(int(expense.amount) for expense in self.expenses)
+        print(f"Total expenses: ${total}")
 
 
 #Main point of entry for the program.        
@@ -43,23 +44,36 @@ def main():
         print("1. Add Expense")
         print("2. Remove Expense")
         print("3. View Expenses")
-        print("4. Add Expenses")
+        print("4. Total Expenses")
         print("5. Exit")
         
         choice = input("Select your choice (1-5): -->> ")
-        if choice == "1" :
+        if choice == "1":
             print("You selected '1. Add Expense', lets get to it!")
-            date = input("First inform the date of the Expense (DD/MM/YYYY) -->> ")
+            date = input("First inform the date of the Expense (YYYY-MM-DD) -->> ")
             description = input("Now inform description of the Expense -->> ")
             amount = input("Lastly, inform the amount ($) of the expense -->>")
             expense = Expense(date, description, amount)      
             tracker.addExpense(expense)
             print("Expense added succesfully!")
         
-        if choice == "2" :  
+        if choice == "2":  
             print("You selected '2. Remove Expense', lets get to it!")
             index = input("Please inform which expense you wish to remove with the index -->> ")
             indexInt = int(index)  # Convert index to integer since it's a string
             tracker.removeExpense(indexInt)
-
+            
+        if choice == "3":
+            print("You selected '3. View Expenses', lets get to it!")
+            tracker.viewExpenses()
+            
+        if choice == "4":
+            print("You selected '4. Total Expenses', lets get to it!")
+            tracker.totalExpenses()
+            
+        if choice == "5":
+            print("Thanks for trying this tracker! hope to see you soon! :) Bye")
+            break
+            
+#Start the program.
 main()
